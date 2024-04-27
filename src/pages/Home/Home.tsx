4bc1a407005f127f7e1384554da56f7e0a807982
VenchasS <49115035+VenchasS@users.noreply.github.com>
2024-04-27 20:23:49 +0300

    theme

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import {createUseStyles} from 'react-jss'
+import {Chat} from '../../common/Chat'
 
 export const Home: React.FC = () => {
   const styles = useStyles()
-  return <div className={styles.root}>HOME</div>
+  return (
+    <div className={styles.root}>
+      <Chat />
+    </div>
+  )
 }
 
 const useStyles = createUseStyles({
-  root: {
-    color: 'green',
-  },
+  root: {},
 })
---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+export const theme = {
+  background: {
+    card: '#2E3233',
+  },
+}
+export type Theme = typeof theme
